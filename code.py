42b27897d0ec04dcd46704f5bee12e1d5580db88
Es2010nz <ethy2010nz@gmail.com>
2026-03-09 12:20:22 +1300

    first day work

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
 #List of the chooseable activeties
- #Activtys can be changed here
-activity_list = [("cultural immersion",5,"easy   ",800),("Kayaking & pancakes",3,"moderate" ,400),(Mountain biking",)]
+#Activtys can be changed here
+activity_list = [("Cultural immersion",5,"easy    ",800),("Kayaking & pancakes",3,"moderate" ,400),("Mountain biking",4,"difficult", 900)]
+print("these are the activites and costs")  
