8bd6ca3c6aca43d0ec77fcd69951ce56eeef6f1b
Es2010nz <ethy2010nz@gmail.com>
2026-03-09 12:02:25 +1300

    trial

---
+++ code.py
@@ -0,0 +1,3 @@
+#List of the chooseable activeties
+ #Activtys can be changed here
+activity_list = [("cultural immersion",5,"easy   ",800),("Kayaking & pancakes",3,"moderate" ,400),(Mountain biking",)]
