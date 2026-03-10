be56d3c73a1d755f3deca885a10960f2b4b01acc
Es2010nz <ethy2010nz@gmail.com>
2026-03-10 13:22:23 +1300

    first iteration no list print

--- code.py
+++ code.py
@@ -1,4 +1,8 @@
 #List of the chooseable activeties
 #Activtys can be changed here
 activity_list = [("Cultural immersion",5,"easy    ",800),("Kayaking & pancakes",3,"moderate" ,400),("Mountain biking",4,"difficult", 900)]
-print("these are the activites and costs")  
+print ("Cultural immersion',5,'easy    ',800")
+print ("Kayaking & pancakes',3,'moderate' ,400")
+print ("Mountain biking',4,'difficult', 900")
+#pick a number
+number_list = 1,2,3
